9896d16e23e9b9b93c11fea8d44ae0f854370493
Abby Malson <august.malson@gmail.com>
2026-05-22 20:51:46 -0600

    fix: update test_helper to use bot_army_library_learning app name

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,4 +4,4 @@ ExUnit.start()
 # Start the application for tests
 # Note: CardStore and SessionManager are NOT started in test mode (@env == :test in application.ex)
 # This allows pure unit tests without database dependencies
-{:ok, _} = Application.ensure_all_started(:bot_army_learning)
+{:ok, _} = Application.ensure_all_started(:bot_army_library_learning)
